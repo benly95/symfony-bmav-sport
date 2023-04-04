4bba22d4508fa21b3e5ff89ac0fe2b976b477d19
benny <bmavtaliane@sedona.fr>
2023-04-04 17:06:37 +0200

    Ajoute de l'admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Categorie;
+use App\Entity\CategorieProduit;
+use App\Entity\Client;
+use App\Entity\Marque;
+use App\Entity\Panier;
+use App\Entity\Produit;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(private readonly AdminUrlGenerator $adminUrlGenerator)
+    {
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $url = $this->adminUrlGenerator->setController(
+            ClientCrudController::class
+        )
+            ->generateUrl();
+
+        return $this->redirect($url);
+
+    }
+
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            // the name visible to end users
+            ->setTitle('Bmav sport')
+            // you can include HTML contents too (e.g. to link to an image)
+            ->setTitle('<img src="..."> Bmav sport Back-Office')
+
+            // by default EasyAdmin displays a black square as its default favicon;
+            // use this method to display a custom favicon: the given path is passed
+            // "as is" to the Twig asset() function:
+            // <link rel="shortcut icon" href="{{ asset('...') }}">
+            ->setFaviconPath('favicon.svg')
+
+            // the domain used by default is 'messages'
+            //->setTranslationDomain('my-custom-domain')
+
+            // there's no need to define the "text direction" explicitly because
+            // its default value is inferred dynamically from the user locale
+            ->setTextDirection('ltr')
+
+            // set this option if you prefer the page content to span the entire
+            // browser width, instead of the default design which sets a max width
+            ->renderContentMaximized()
+
+            // set this option if you prefer the sidebar (which contains the main menu)
+            // to be displayed as a narrow column instead of the default expanded design
+            //->renderSidebarMinimized()
+
+            // by default, users can select between a "light" and "dark" mode for the
+            // backend interface. Call this method if you prefer to disable the "dark"
+            // mode for any reason (e.g. if your interface customizations are not ready for it)
+            ->disableDarkMode()
+
+            // by default, all backend URLs are generated as absolute URLs. If you
+            // need to generate relative URLs instead, call this method
+            ->generateRelativeUrls()
+
+            // set this option if you want to enable locale switching in dashboard.
+            // IMPORTANT: this feature won't work unless you add the {_locale}
+            // parameter in the admin dashboard URL (e.g. '/admin/{_locale}').
+            // the name of each locale will be rendered in that locale
+            // (in the following example you'll see: "English", "Polski")
+            ->setLocales(['fr'])
+            // to customize the labels of locales, pass a key => value array
+            // (e.g. to display flags; although it's not a recommended practice,
+            // because many languages/locales are not associated to a single country)
+            ->setLocales([
+                'fr'
+            ]);
+
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Clients & commande');
+        yield MenuItem::linkToCrud('Clients', 'fa fa-tags', Client::class);
+        yield MenuItem::linkToCrud('Panier', 'fa fa-tags', Panier::class);
+        yield MenuItem::section('Produit ');
+        yield MenuItem::LinkToCrud('Produit', 'fa fa-tags', Produit::class);
+        yield MenuItem::LinkToCrud('Marque', 'fa fa-tags', entityFqcn: Marque::class);
+        yield MenuItem::linkToCrud('Categorie', 'fa fa-tags', entityFqcn: Categorie::class);
+        yield MenuItem::linkToCrud('Categorie Produit', 'fa fa-tags', entityFqcn: CategorieProduit::class);
+
+
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
